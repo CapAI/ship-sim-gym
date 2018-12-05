e6a1c68f9ece0e2228d10f6bd408ef082ad4540c
Gerard Simons <sjrasimons@gmail.com>
2018-12-05 21:02:53 +0100

    Fixed rllib PPO to work with new env config

--- train/rllib/ppo.py
+++ train/rllib/ppo.py
@@ -8,37 +8,44 @@ from ray import tune
 from ray.tune.schedulers import PopulationBasedTraining
 
 from ship_gym.game import ShipGame
+
+from ship_gym.config import GameConfig, EnvConfig
 from ship_gym.ship_env import ShipEnv
+
+import multiprocessing
 
 if __name__ == "__main__":
 
     # register_env(env_creator_name, lambda config: ship_gym = gym.make('ShipSim-v0'))
 
+    game_config = GameConfig
+    game_config.FPS = 100000
+    game_config.SPEED = 40
+    game_config.BOUNDS = (1000, 1000)
+
     ray.init(num_gpus=1)
 
     def env_creator(env_config):
 
-        game = ShipGame(speed=20, fps=10000, bounds=(800, 800))
-        env = ShipEnv(game, env_config)
-
-        print(env_config)
+        env_config = EnvConfig
+        env = ShipEnv(game_config, env_config)
 
         return env
 
     experiments = {
-        "find-lr": {
+        "shipgym_best": {
             "run": "PPO",
             "stop": {
-                "time_total_s":180
+                "time_total_s": 12 * 60 * 60 # 12 hours
             },
             "env": "ship-gym-v1",
             "config": {
-                "num_gpus": 1,
-                "num_workers" : 10,
-                "lr" : tune.grid_search([0.001, 0.0001, 0.00001]),
-                "env_config": {
-                    "n_goals": 5
-                }
+            	"num_gpus": 1,
+                "num_workers" : multiprocessing.cpu_count() - 1,
+                "num_sgd_iter" :  10,
+                "sgd_minibatch_size" : 2048,
+                "train_batch_size" : 10000,
+                "lr_schedule" : [[0, 0.001], [5e6, 0.0001], [1e7, 0.00001]]
             },
         },
     }
